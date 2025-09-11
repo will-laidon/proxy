443f0213180abc8b1a95287cfbe5008a7aac96dd
Will Dao <will.dao@laidon.com>
2025-09-11 10:39:16 +0700

    refactor: extract url from headers

--- src/app.ts
+++ src/app.ts
@@ -3,42 +3,50 @@ import axios from 'axios'
 import config from 'dotenv'
 import * as _ from 'lodash'
 import chalk from 'chalk'
-import { PORT, SV_URL } from './config'
 
 config.config()
 
 const app: Application = express()
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
-app.use(express.json())
+// app.use('/srv-requestor/RequestorService/getMigrationLogs', (req: Request, res: Response) => {
+//   return res.json()
+// })
+
+const PORT = 3000
 
 app.use('/*', async (req: Request, res: Response) => {
   let request
 
   try {
-    console.log(chalk.magenta('[Proxy]', chalk.cyan(`${SV_URL}${req.originalUrl}`)))
+    const headers ={
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "en",
+    "application-interface-key": "52ve7fwy",
+    "content-type": "application/json",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Chromium\";v=\"140\", \"Not=A?Brand\";v=\"24\", \"Brave\";v=\"140\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "sec-gpc": "1",
+    "x-correlation-id": "leo.phan@laidon.com",
+    "x-csrf-token": "48433e9cb967eacc-UlDhtA1P0ZMR4UalB4X2AN_hZyc",
+    "cookie": "JSESSIONID=s%3A5CYpnqi4903yiSeQhv5cD3yE9sb2m_i7.aX0Uo5ogdtXwMroUG5UYScE7O%2FCzlMx6Z1bLyNTqtiM; __VCAP_ID__=434a2d14-68f7-4e0f-40eb-ee44",
+    "Referer": "https://edf-dev-simplemdg-web.cfapps.us10-001.hana.ondemand.com/main/index.html"
+  }
 
+    const SV_URL = new URL(headers.Referer).origin
+
+    console.log(chalk.magenta('[Proxy]', chalk.cyan(`${SV_URL}${req.originalUrl}`)))
     request = await axios.request({
       method: req.method,
       url: `${SV_URL}${req.originalUrl}`,
       data: req.body,
-      headers: {
-        accept: 'application/json, text/plain, */*',
-        'accept-language': 'en',
-        'application-interface-key': '52ve7fwy',
-        'content-type': 'application/json',
-        priority: 'u=1, i',
-        'sec-ch-ua': '"Not;A=Brand";v="99", "Brave";v="139", "Chromium";v="139"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'sec-gpc': '1',
-        'x-correlation-id': 'butterball.dev@laidon.com',
-        'x-csrf-token': '202f9a722d206bb9-eEP3jsSmqgNmm6-GEfX8ZEkuadU',
-        cookie: 'JSESSIONID=s%3ArxnXFeojaI9J2igE2EY9sMzthP_6wZyl.nIYZTeviUozln%2BRvbib9KxMo%2FY0PrbzLl68ZfV2dlrg; __VCAP_ID__=44cb6fe2-5f05-47ce-6da2-341f',
-        Referer: 'https://butterball-dev-simplemdg-web.cfapps.us21.hana.ondemand.com/main/index.html',
-      },
+      headers,
     })
   } catch (error) {
     const status = error?.response?.status || 500
@@ -61,5 +69,4 @@ app.use('/*', async (req: Request, res: Response) => {
 
 app.listen(PORT, (): void => {
   console.log(chalk.green('SERVER IS UP ON PORT:'), chalk.yellow(PORT))
-  console.log(chalk.white('SV_URL:'), chalk.yellow(SV_URL))
 })
--- src/config.ts
+++ src/config.ts
@@ -1,32 +1,36 @@
-export const PORT = 3001
+export const PORT = 3000
 
-// export const SV_URL = 'https://central-hub-simplemdg-web.cfapps.us10.hana.ondemand.com' // ENV=Multiple dev
-// export const SV_URL='https://manual-test-ecc-simplemdg-web.cfapps.eu10.hana.ondemand.com'
-// export const SV_URL='https://manual-test-s4-simplemdg-web.cfapps.eu10.hana.ondemand.com'
-// export const SV_URL='https://demo-s4-simplemdg-web.cfapps.eu10.hana.ondemand.com'
-// export const SV_URL='https://sukandadjaya-qas-simplemdg-web.cfapps.ap21.hana.ondemand.com'
-// export const SV_URL='https://manual-s4-sandbox-simplemdg-web.cfapps.eu10.hana.ondemand.com'
-// export const SV_URL='https://single-dev-simplemdg-web.cfapps.br10.hana.ondemand.com'
-// export const SV_URL='https://franmax-dev-simplemdg-web.cfapps.eu10-004.hana.ondemand.com'
 // export const SV_URL='https://aliaxis-dev-simplemdg-web.cfapps.eu20-001.hana.ondemand.com'
 // export const SV_URL='https://aliaxis-qas-simplemdg-web.cfapps.eu20-001.hana.ondemand.com'
-// export const SV_URL='https://jlr-dev-simplemdg-web.cfapps.eu10-004.hana.ondemand.com'
-// export const SV_URL='https://smdg-s4-dev-simplemdg-web.cfapps.br10.hana.ondemand.com'
-// export const SV_URL='https://metrie-quality-simplemdg-web.cfapps.us20.hana.ondemand.com'
-// export const SV_URL='https://single-ams-simplemdg-web.cfapps.br10.hana.ondemand.com'
-// export const SV_URL='https://single-dev-simplemdg-web.cfapps.br10.hana.ondemand.com' // ENV=single-experiment
-// export const SV_URL='https://smdg-prestage-simplemdg-web.cfapps.br10.hana.ondemand.com'
-// export const SV_URL='https://metrie-dev-simplemdg-web.cfapps.us10-001.hana.ondemand.com'
-// export const SV_URL='https://smdg-performance-simplemdg-web.cfapps.br10.hana.ondemand.com' // ENV=franmax-automation
-// export const SV_URL='https://smdg-2-demo-simplemdg-web.cfapps.br10.hana.ondemand.com' // ENV=Demo 2
-// export const SV_URL='https://single-cgs-simplemdg-web.cfapps.br10.hana.ondemand.com' // ENV=single-central
-// export const SV_URL='https://diana-dev-simplemdg-web.cfapps.br10.hana.ondemand.com'
-// export const SV_URL='https://multiple-qas-simplemdg-web.cfapps.br10.hana.ondemand.com'
-// export const SV_URL = 'https://smdg-s4-qas-simplemdg-web.cfapps.br10.hana.ondemand.com'
-// export const SV_URL='https://asp-dev-simplemdg-web.cfapps.us11.hana.ondemand.com'
-// export const SV_URL='https://cytiva-dev-simplemdg-web.cfapps.us10-001.hana.ondemand.com'
 // export const SV_URL='https://amman-mineral-dev-simplemdg-web.cfapps.ap21.hana.ondemand.com'
-// export const SV_URL = 'https://endeavour-energy-dev-2023-simplemdg-web.cfapps.ap10.hana.ondemand.com'
-// export const SV_URL = 'https://endeavour-energy-dev-1909-simplemdg-web.cfapps.ap10.hana.ondemand.com'
-// export const SV_URL = 'https://bulla-dairy-dev-simplemdg-web.cfapps.ap10.hana.ondemand.com'
-export const SV_URL = 'https://butterball-dev-simplemdg-web.cfapps.us21.hana.ondemand.com'
+// export const SV_URL='https://asp-dev-simplemdg-web.cfapps.us11.hana.ondemand.com'
+// export const SV_URL='https://bulla-dairy-dev-simplemdg-web.cfapps.ap10.hana.ondemand.com'
+// export const SV_URL='https://butterball-dev-simplemdg-web.cfapps.us21.hana.ondemand.com'
+// export const SV_URL='https://central-hub-simplemdg-web.cfapps.us10.hana.ondemand.com' // ENV=Multiple dev
+// export const SV_URL='https://cytiva-dev-simplemdg-web.cfapps.us10-001.hana.ondemand.com'
+// export const SV_URL='https://demo-s4-simplemdg-web.cfapps.eu10.hana.ondemand.com'
+// export const SV_URL='https://diana-dev-simplemdg-web.cfapps.br10.hana.ondemand.com'
+// export const SV_URL='https://endeavour-energy-dev-1909-simplemdg-web.cfapps.ap10.hana.ondemand.com'
+// export const SV_URL='https://endeavour-energy-dev-2023-simplemdg-web.cfapps.ap10.hana.ondemand.com'
+// export const SV_URL='https://franmax-dev-simplemdg-web.cfapps.eu10-004.hana.ondemand.com'
+// export const SV_URL='https://franmax-qas-simplemdg-web.cfapps.eu10-004.hana.ondemand.com'
+// export const SV_URL='https://jlr-dev-simplemdg-web.cfapps.eu10-004.hana.ondemand.com'
+// export const SV_URL='https://kawan-lama-simplemdg-web.cfapps.br10.hana.ondemand.com'
+// export const SV_URL='https://manual-s4-sandbox-simplemdg-web.cfapps.eu10.hana.ondemand.com'
+// export const SV_URL='https://manual-test-ecc-simplemdg-web.cfapps.eu10.hana.ondemand.com'
+// export const SV_URL='https://manual-test-s4-simplemdg-web.cfapps.eu10.hana.ondemand.com'
+// export const SV_URL='https://metrie-dev-simplemdg-web.cfapps.us10-001.hana.ondemand.com'
+// export const SV_URL='https://metrie-quality-simplemdg-web.cfapps.us20.hana.ondemand.com'
+// export const SV_URL='https://multiple-qas-simplemdg-web.cfapps.br10.hana.ondemand.com'
+// export const SV_URL='https://pmeleven-dev-simplemdg-web.cfapps.br10.hana.ondemand.com'
+// export const SV_URL='https://single-ams-simplemdg-web.cfapps.br10.hana.ondemand.com'
+// export const SV_URL='https://single-cgs-simplemdg-web.cfapps.br10.hana.ondemand.com' // ENV=single-central
+// export const SV_URL='https://single-dev-simplemdg-web.cfapps.br10.hana.ondemand.com'
+// export const SV_URL='https://single-dev-simplemdg-web.cfapps.br10.hana.ondemand.com' // ENV=single-experiment
+// export const SV_URL='https://smdg-2-demo-simplemdg-web.cfapps.br10.hana.ondemand.com' // ENV=Demo 2
+// export const SV_URL='https://smdg-performance-simplemdg-web.cfapps.br10.hana.ondemand.com' // ENV=franmax-automation
+// export const SV_URL='https://smdg-prestage-simplemdg-web.cfapps.br10.hana.ondemand.com'
+// export const SV_URL='https://smdg-s4-dev-simplemdg-web.cfapps.br10.hana.ondemand.com'
+// export const SV_URL='https://smdg-s4-qas-simplemdg-web.cfapps.br10.hana.ondemand.com'
+// export const SV_URL='https://sukandadjaya-qas-simplemdg-web.cfapps.ap21.hana.ondemand.com'
+// export const SV_URL='https://vicor-dev-simplemdg-web.cfapps.us10.hana.ondemand.com'
