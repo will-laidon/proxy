d828fdbbf85c47d5f3073d60a69717200f89cf0e
Will Dao <will.dao@laidon.com>
2026-04-03 16:39:05 +0700

    remove test file

--- src/app.ts
+++ src/app.ts
@@ -18,82 +18,13 @@ enum Ports {
 }
 
 // app.use('/srv-main/MainService/RequestAttachments', async (req: Request, res: Response) => {
-//   // set OData-Version 4.0
-//   res.setHeader('OData-Version', '4.0')
-//   return res.status(200).send({
-//   "@odata.context": "$metadata#RequestAttachments",
-//   "value": [
-//     {
-//       "ID": "e5b9c861-2c3e-4dfa-80d7-f1340b3415ee",
-//       "createdAt": "2026-02-12T04:04:49.634Z",
-//       "createdBy": "admin.s4cloud@laidon.com",
-//       "endTime": "2000-01-01T00:00:00.000Z",
-//       "file@odata.mediaContentType": null,
-//       "fileID": "SIMPLEMDG_PublicCloud/MasterData/CR0000011352/20260212/1006632_ARTM.01_2026-02-04T10_38_20.568Z.xlsx",
-//       "fileName": "1006632_ARTM.01_2026-02-04T10_38_20.568Z.xlsx",
-//       "fileSize": "55012",
-//       "fileType": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-//       "isActive": true,
-//       "isDraft": false,
-//       "isMarkDelete": false,
-//       "itemID": null,
-//       "modifiedAt": "2026-02-12T04:10:11.376Z",
-//       "modifiedBy": "privileged",
-//       "reqID": "CR0000011352",
-//       "storageType": "6",
-//       "type": "RequestAttachment",
-//       "url": "https://laidonnam.sharepoint.com/sites/SitepointUpload"
-//     },
-//     {
-//       "ID": "b80f6fae-f0c7-4244-9f20-81800d8aa0d2",
-//       "createdAt": "2026-02-12T04:04:48.967Z",
-//       "createdBy": "admin.s4cloud@laidon.com",
-//       "endTime": "2000-01-01T00:00:00.000Z",
-//       "file@odata.mediaContentType": null,
-//       "fileID": "SIMPLEMDG_PublicCloud/MasterData/CR0000011352/20260212/2662501_SA_BP02_2026-01-29T04_06_31.859Z.xlsx",
-//       "fileName": "2662501_SA_BP02_2026-01-29T04_06_31.859Z.xlsx",
-//       "fileSize": "30532",
-//       "fileType": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-//       "isActive": true,
-//       "isDraft": false,
-//       "isMarkDelete": false,
-//       "itemID": null,
-//       "modifiedAt": "2026-02-12T04:10:11.376Z",
-//       "modifiedBy": "privileged",
-//       "reqID": "CR0000011352",
-//       "storageType": "6",
-//       "type": "RequestAttachment",
-//       "url": "https://laidonnam.sharepoint.com/sites/SitepointUpload"
-//     },
-//     {
-//       "ID": "f7f807fb-9b7d-42cb-a955-18bc36b193cc",
-//       "createdAt": "2026-02-12T04:04:47.523Z",
-//       "createdBy": "admin.s4cloud@laidon.com",
-//       "endTime": "2000-01-01T00:00:00.000Z",
-//       "file@odata.mediaContentType": null,
-//       "fileID": "SIMPLEMDG_PublicCloud/MasterData/CR0000011352/20260212/2223919_UIMDCLASSTYPE_2026-02-11T11_50_17.195Z_2026-02-11T11_50_17.195Z.xlsx",
-//       "fileName": "2223919_UIMDCLASSTYPE_2026-02-11T11_50_17.195Z_2026-02-11T11_50_17.195Z.xlsx",
-//       "fileSize": "19564",
-//       "fileType": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-//       "isActive": true,
-//       "isDraft": false,
-//       "isMarkDelete": false,
-//       "itemID": null,
-//       "modifiedAt": "2026-02-12T04:10:11.376Z",
-//       "modifiedBy": "privileged",
-//       "reqID": "CR0000011352",
-//       "storageType": "6",
-//       "type": "RequestAttachment",
-//       "url": "https://laidonnam.sharepoint.com/sites/SitepointUpload"
-//     }
-//   ]
-// })
+//   return res.status(200).send()
 // })
 
 const PORT = Number(process.env.PORT) || Ports.A
 console.log('process.env.PORT: ', process.env.PORT)
 // .filter(e => e.tableName.length > 25).map(e => e.tableName)
-let raw = String.raw`fetch("https://olymel-dev-simplemdg-web.cfapps.ca10.hana.ondemand.com/srv-approver/ApproverService/myInbox", {
+let raw = String.raw`fetch("https://multiple-erp-dev-simplemdg-web.cfapps.br10.hana.ondemand.com/srv-process/CommonProcessService/getBusinessRequest", {
   "headers": {
     "accept": "application/json, text/plain, */*",
     "accept-language": "en-US",
@@ -106,42 +37,14 @@ let raw = String.raw`fetch("https://olymel-dev-simplemdg-web.cfapps.ca10.hana.on
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-origin",
-    "x-correlation-id": "olymel.dev@laidon.com",
-    "x-csrf-token": "b94264e3946ac43d-gA8wWBc_uamoZOHSayXEhkilT74",
-    "cookie": "JSESSIONID=s%3AEt_UJnw7VI4_s5iptDyhLOcmMlkp4CjK.TY%2BRkh0bofkVMnVOSWDEQxRpVIYnMJak4qVQWxNxEnY; __VCAP_ID__=aac3f821-e406-40d9-7997-a773",
-    "Referer": "https://olymel-dev-simplemdg-web.cfapps.ca10.hana.ondemand.com/main/index.html"
+    "x-correlation-id": "multiple.demo@laidon.com",
+    "x-csrf-token": "520d3c3e5959f9c3-ZRkh36TZPxks5Nke2FXNC2F8QFk",
+    "cookie": "__VCAP_ID__=6649d15b-f948-46a6-4aa7-43a4; JSESSIONID=s%3ALorpjfBSBkZmdAlmSiRpRO7w-lmTRy3S.Z2QNLknrNMTB5a5KAlP4ANHJ%2FejXwTVcAYkjZ3PENEk",
+    "Referer": "https://multiple-erp-dev-simplemdg-web.cfapps.br10.hana.ondemand.com/main/index.html"
   },
-  "body": "{\"myInbox\":{\"category\":\"DIRECT\",\"status\":\"\",\"searchString\":\"\",\"top\":20,\"skip\":0,\"offSet\":0,\"isScheduled\":false,\"sortBy\":[\"\",\"DESC\"],\"groupBy\":\"approvalStatus\"}}",
+  "body": "{\"businessRequest\":{\"reqID\":\"\",\"tempID\":\"\",\"objectID\":\"\",\"createdAtFrom\":\"\",\"createdAtTo\":\"\",\"modifiedAtFrom\":\"\",\"modifiedAtTo\":\"\",\"reason\":\"\",\"status\":\"ALL\",\"objectType\":\"\",\"slaID\":\"\",\"isType\":\"STATUS\",\"top\":20,\"skip\":0,\"offSet\":0,\"searchString\":\"\",\"createdBy\":[\"multiple.demo@laidon.com\"],\"sortBy\":[\"modifiedAt\",\"DESC\"]}}",
   "method": "POST"
 });`
-
-
-// if (PORT === Ports.A) {
-//   raw = String.raw`fetch("https://pre-stage-2-simplemdg-web.cfapps.br10.hana.ondemand.com/srv-process/CommonProcessService/getBusinessRequest", {
-//   "headers": {
-//     "accept": "application/json, text/plain, */*",
-//     "accept-language": "en-US",
-//     "application-interface-key": "52ve7fwy",
-//     "cache-control": "no-cache",
-//     "content-type": "application/json",
-//     "pragma": "no-cache",
-//     "priority": "u=1, i",
-//     "sec-ch-ua": "\"Not:A-Brand\";v=\"99\", \"Microsoft Edge\";v=\"145\", \"Chromium\";v=\"145\"",
-//     "sec-ch-ua-mobile": "?0",
-//     "sec-ch-ua-platform": "\"Windows\"",
-//     "sec-fetch-dest": "empty",
-//     "sec-fetch-mode": "cors",
-//     "sec-fetch-site": "same-origin",
-//     "x-correlation-id": "rina.ksor@laidon.com",
-//     "x-csrf-token": "442b135d22a8c629-VFdgSN3p106-QXJ-CDqsQ1dP82A",
-//     "cookie": "JSESSIONID=s%3AANDbWbrpQXYYAQM2D002OYWCAdHsvxbZ.Z8hry5Csqvi6IqwN6tIgswFa%2Beecmdy%2BbkD1CZvtA9o; __VCAP_ID__=3c665d87-73ce-48a7-737c-d74b",
-//     "Referer": "https://pre-stage-2-simplemdg-web.cfapps.br10.hana.ondemand.com/main/index.html"
-//   },
-//   "body": "{\"businessRequest\":{\"reqID\":\"\",\"tempID\":\"\",\"objectID\":\"\",\"createdAtFrom\":\"\",\"createdAtTo\":\"\",\"modifiedAtFrom\":\"\",\"modifiedAtTo\":\"\",\"reason\":\"\",\"status\":\"ALL\",\"isScheduled\":false,\"objectType\":\"\",\"priority\":\"\",\"isType\":\"STATUS\",\"top\":20,\"skip\":0,\"offSet\":0,\"searchString\":\"\",\"createdBy\":[\"rina.ksor@laidon.com\"],\"sortBy\":[\"modifiedAt\",\"DESC\"]}}",
-//   "method": "POST"
-// });`
-// }
-
 
 const headers = extractHeadersFromAxiosCode(raw)
 const SV_URL = new URL(headers.Referer).origin
